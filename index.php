11601addcf22dd7801974935dfce46582e0296a8
webnetkz <8gg001@gmail.com>
2019-03-18 10:21:15 +0700

    start project

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>title</title>
+
+        <meta charset="UTF-8">
+        <meta name="theme-color" content="rgb(0, 173, 239)">
+        <meta name="author" content="TOO WebNet">
+        <meta name="description" content="RetactorPhoto by WebNet">
+        <meta name="keywords" content="RedactorPhoto">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+
+        <link rel="shortcut icon" href="/public/images/miniLogoWebnet.png" type="image/png">
+        <link rel="stylesheet" href="/public/styles/style.css">
+        <link rel="stylesheet" href="/public/styles/mobileStyle.css">
+        <link rel="manifest" href="/manifest.json">
+        
+    </head>
+
+    <body>
+        
+        <script>
+             // Проверка на поддержку service worker
+            if('serviceWorker' in navigator) {
+                navigator.serviceWorker
+                    .register('/sw.js')
+                    .then(function() { console.log("Service Worker Registered"); });
+            }
+        </script>
+        <script src="/public/scripts/main.js"></script>
+    </body>
+</html>
