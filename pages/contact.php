b58372b571e381ea7d3452cf9921cd3f942c9387
webnetkz <8gg001@gmail.com>
2019-03-23 16:30:44 +0700

    append maps for page contact

--- pages/contact.php
+++ pages/contact.php
@@ -6,6 +6,10 @@
         <?php
             require_once '../components/header.php';
         ?>
+
+
+<a class="dg-widget-link" href="http://2gis.kz/almaty/firm/70000001024623698/center/76.90906047821046,43.23440117447717/zoom/16?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть на карте Алматы</a><div class="dg-widget-link"><a href="http://2gis.kz/almaty/firm/70000001024623698/photos/70000001024623698/center/76.90906047821046,43.23440117447717/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=photos">Фотографии компании</a></div><div class="dg-widget-link"><a href="http://2gis.kz/almaty/center/76.909058,43.234091/zoom/16/routeTab/rsType/bus/to/76.909058,43.234091╎ЭМИРМЕД, медицинский центр?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти проезд до ЭМИРМЕД, медицинский центр</a></div><script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script><script charset="utf-8">new DGWidgetLoader({"width":600,"height":600,"borderColor":"rgb(255, 255 ,255)","pos":{"lat":43.23440117447717,"lon":76.90906047821046,"zoom":16},"opt":{"city":"almaty"},"org":[{"id":"70000001024623698"}]});</script><noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в настройках вашего браузера.</noscript>
+
             <pre>
 
 
@@ -69,7 +73,7 @@
 
 
 
-            
+
             </pre>
         <?php
             require_once '../components/footer.php';
